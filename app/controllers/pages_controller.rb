35a6f3162ebfdb6e1ed3302e3f48d71afc1c6803
Peter3452 <p1312402@gmail.com>
2022-08-17 16:23:38 +0300

    добавлены скоупы для рума и юзера, добавлено отображение всех румов и юзеров кроме скоупов

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,4 @@
-class PagesController < ApplicationController
+class PagesController < ApplicationController # для отображения страницы входа !!!
   def index
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :rooms
+
   devise_scope :user do 
     get 'users', to: 'devise/sessions#new'
   end
